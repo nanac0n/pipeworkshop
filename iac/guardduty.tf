9e007bf836312ff4ef2c8b269ecaa83d43a70dd1
Jhsysng <screenshot161@gmail.com>
2023-12-30 16:12:37 +0900

    feat: guardduty iac, spring lambda

---
+++ iac/guardduty.tf
@@ -0,0 +1,21 @@
+/*resource "aws_guardduty_detector" "DevSecOpsDetector" {
+  enable = true
+
+  datasources {
+    s3_logs {
+      enable = true
+    }
+    kubernetes {
+      audit_logs {
+        enable = false
+      }
+    }
+    malware_protection {
+      scan_ec2_instance_with_findings {
+        ebs_volumes {
+          enable = true
+        }
+      }
+    }
+  }
+}*/
