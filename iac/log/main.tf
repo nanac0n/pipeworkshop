6dcba97d36a61208f651c25d8374eaa0297c5db4
Jhsysng <screenshot161@gmail.com>
2023-12-30 17:14:42 +0900

    refactor: s3

--- iac/log/main.tf
+++ iac/log/main.tf
@@ -41,3 +41,8 @@ resource "aws_s3_bucket" "tf-aws-s3-bucket"{
   bucket = "aws-waf-logs-bucket"
   force_destroy = true
 }
+
+resource "aws_s3_bucket" "tf-aws-s3-bucket" {
+  bucket        = "aws-gd-logs-bucket"
+  force_destroy = true
+}
