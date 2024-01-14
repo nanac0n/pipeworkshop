b635a4ec0ac39da934cfcc077b4c761dc8e4067b
nanac0n <lina01@ajou.ac.kr>
2024-01-14 15:30:26 +0900

    구버전 테라폼 코드 제거

--- iac/log/deploy-test/modules/terraform-aws-btlabz-arch-ref-ec2-codedeploy-module/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "app_name" {
-  description = "CodeDeploy Application Name"
-  value       = aws_codedeploy_app.main.name
-}
-
-output "codedeploy_deployment_group_arn" {
-  value = aws_codedeploy_deployment_group.main.arn
-}
-
--- iac/log/deploy-test/modules/terraform-aws-btlabz-arch-ref-ec2-codedeploy-module/variables.tf
+++
@@ -1,9 +0,0 @@
-variable "tags" {
-  description = "Additional tags. E.g. environment, backup tags etc"
-  type        = map
-  default     = {}
-}
-
-variable "role_arn" {
-  type = string
-}
